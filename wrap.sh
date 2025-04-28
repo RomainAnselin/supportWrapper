087ab38b8880badabe326da7d4498de8c23b8582
PeterGilroy <78808178+PeterGilroy@users.noreply.github.com>
2025-04-28 15:18:58 +0100

    Update wrap.sh

--- wrap.sh
+++ wrap.sh
@@ -4,21 +4,36 @@ debug=0
 
 usage() {
   if [ $# -ne 1 ]; then
-    echo "Usage: $0 [-s] [-p] [-h] <Path to Opscenter diag>"
-    echo "  -s   solr data"
-    echo "  -p   GC per node"
-    # TODO: Reset -r option to erase existing parsed data if you want to re-run with different options
+    echo "Usage: $0 [-s] [-p] [-g] [-d] [-w] [-m {tar.gz}] [-t {ticketid}] [-h] <Path to Opscenter diag>"
+    echo "  -s            solr data"
+    echo "  -p            GC per node"
+    echo "  -g            greps script"
+    echo "  -d            Diag Viewer db creation"
+    echo "  -m {tar.gz}   MonteCristo Services diag execution"
+    echo "  -t {ticketid} Ticket number. Necessary for Montecristo"
+    echo "  -w            Do not wrap Nibbler output"
+    # TODO (or not, I dont like scripting removal): Reset -r option to erase existing parsed data if you want to re-run with different options
     echo "  -h   show this help"
     exit 1
   fi
 }
 
-while getopts "sph" option; do
+while getopts "spgdmw:t:h" option; do
   case "${option}" in
     s) echo "Solr parsing requested"
        solrparse=1 ;;
     p) echo "Node per node parsing"
        pernode=1  ;;
+    g) echo "Greps requested"
+       slgreps=1  ;;
+    d) echo "Generating diag-viewer"
+       diagv=1  ;;
+    m) echo "Generating Montecristo"
+       diagtgz="$OPTARG"
+       montecris=1  ;;
+    t) ticketid="$OPTARG" ;;
+    w) echo "Do not wrap Nibbler output selected"
+       nowrap=1  ;;
     h) echo "Showing help"
        usage ;;
   esac
@@ -46,27 +61,28 @@ fi
 prep() {
   mkdir "$opscdiag"/wrapper
   cp "$template"/index.html "$opscdiag"/wrapper/index.html
-  cp "$template"/datastax.png "$opscdiag"/wrapper/datastax.png
+  #cp "$template"/datastax.png "$opscdiag"/wrapper/datastax.png
+  cp "$template"/ds.svg "$opscdiag"/wrapper/ds.svg
 }
 
 linkname() {
-  if [[ "$i" == "Cluster_Configuration_Summary.out" ]]; then link=Summary
-  elif [[ "$i" == "Node_Configuration_Files_Info.out" ]]; then link=Conf
-  elif [[ "$i" == "Node_Info.out" ]]; then link=Info
-  elif [[ "$i" == "Node_Resource_Usage_Info.out" ]]; then link=Resource
-  elif [[ "$i" == "Node_Status.out" ]]; then link=Status
-  elif [[ "$i" == "System_Log_Event_Info.out" ]]; then link=SystemLog
-  elif [[ "$i" == "Table_Statistics.out" ]]; then link=TableStats
-  elif [[ "$i" == "Thread_Pool_Statistics.out" ]]; then link=ThreadPool
-  elif [[ "$i" == "sperfgc.txt" ]]; then link=GC
-  elif [[ "$i" == "sperfgeneral.txt" ]]; then link=General
-  elif [[ "$i" == "sperfstatuslog.txt" ]]; then link=StatusLogger
-  elif [[ "$i" == "sperfslow.txt" ]]; then link=SlowQueries
-  elif [[ "$i" == "sperfschema.txt" ]]; then link=Schema
-  elif [[ "$i" == "sperfnodegc.txt" ]]; then link=NodeGC
-  elif [[ "$i" == "sperfnodestatuslog.txt" ]]; then link=NodeStatusLogger
-  elif [[ "$i" == "sperfsolrcache.txt" ]]; then link=SolrCache
-  elif [[ "$i" == "sperfsolrscore.txt" ]]; then link=SolrScore
+  if [[ "$i" == "Cluster_Configuration_Summary" ]]; then link=Summary
+  elif [[ "$i" == "Node_Configuration_Files_Info" ]]; then link=Conf
+  elif [[ "$i" == "Node_Info" ]]; then link=Info
+  elif [[ "$i" == "Node_Resource_Usage_Info" ]]; then link=Resource
+  elif [[ "$i" == "Node_Status" ]]; then link=Status
+  elif [[ "$i" == "System_Log_Event_Info" ]]; then link=SystemLog
+  elif [[ "$i" == "Table_Statistics" ]]; then link=TableStats
+  elif [[ "$i" == "Thread_Pool_Statistics" ]]; then link=ThreadPool
+  elif [[ "$i" == "sperf2gc.txt" ]]; then link=GC
+  elif [[ "$i" == "sperf1general.txt" ]]; then link=General
+  elif [[ "$i" == "sperf3statuslog.txt" ]]; then link=StatusLogger
+  elif [[ "$i" == "sperf4slow.txt" ]]; then link=SlowQueries
+  elif [[ "$i" == "sperf5schema.txt" ]]; then link=Schema
+  elif [[ "$i" == "sperf6nodegc.txt" ]]; then link=NodeGC
+  elif [[ "$i" == "sperf7nodestatuslog.txt" ]]; then link=NodeStatusLogger
+  elif [[ "$i" == "sperf8solrcache.txt" ]]; then link=SolrCache
+  elif [[ "$i" == "sperf9solrscore.txt" ]]; then link=SolrScore
   else link=$i
 fi
 # echo $link
@@ -99,54 +115,96 @@ if [[ "$debug" == "1" ]]; then
   printf 'DEBUG\t Pyornotpy: %s\n' "$pyornotpy"
   echo "DEBUG  Command: "$pythonpath" "$sperfpath" -x -d "$subdiag" > ./wrapper/sperfgeneral.txt"
 fi
+
 if [[ "$pyornotpy" == "1" ]]; then
-  echo "Sperf summary"
-  "$pythonpath" "$sperfpath" -x -d "$subdiag" > ./wrapper/sperfgeneral.txt &
-  echo "Sperf GC analysis"
-  "$pythonpath" "$sperfpath" -x -d "$subdiag" core gc > ./wrapper/sperfgc.txt &
-  echo "Sperf StatusLogger"
-  "$pythonpath" "$sperfpath" -x -d "$subdiag" core statuslogger > ./wrapper/sperfstatuslog.txt &
-  echo "Sperf Slow Query"
-  "$pythonpath" "$sperfpath" -x core slowquery -d "$subdiag" > ./wrapper/sperfslow.txt &
-  echo "Sperf Schema"
-  "$pythonpath" "$sperfpath" -x core schema -d "$subdiag" > ./wrapper/sperfschema.txt &
-  if [[ "$pernode" == 1 ]]; then
-    echo "Sperf GC per node"
-    "$pythonpath" "$sperfpath" -x -d "$subdiag" core gc -r nodes > ./wrapper/sperfnodegc.txt &
-    echo "Sperf StatusLogger per node"
-    "$pythonpath" "$sperfpath" -x -d "$subdiag" core statuslogger -r histogram > ./wrapper/sperfnodestatuslog.txt &
-  fi
-  if [[ "$solrparse" == 1 ]]; then
-    echo "Sperf Solr Filtercache"
-    "$pythonpath" "$sperfpath" -x search filtercache -d "$subdiag" > ./wrapper/sperfsolrcache.txt &
-    echo "Sperf Solr Queryscore"
-    "$pythonpath" "$sperfpath" -x search queryscore -d "$subdiag" > ./wrapper/sperfsolrscore.txt &
-  fi
+  sperfcmd="$pythonpath $sperfpath"
+  printf 'sperfcmd: %s \n' "$sperfcmd"
 else
-  echo "Sperf summary"
-  "$sperfpath" -x -d "$subdiag" > ./wrapper/sperfgeneral.txt &
-  echo "Sperf GC analysis"
-  "$sperfpath" -x -d "$subdiag" core gc > ./wrapper/sperfgc.txt &
-  echo "Sperf StatusLogger"
-  "$sperfpath" -x -d "$subdiag" core statuslogger > ./wrapper/sperfstatuslog.txt &
-  echo "Sperf Slow Query"
-  "$sperfpath" -x core slowquery -d "$subdiag" > ./wrapper/sperfslow.txt &
-  echo "Sperf Schema"
-  "$sperfpath" -x core schema -d "$subdiag" > ./wrapper/sperfschema.txt &
-  if [[ "$pernode" == 1 ]]; then
-    echo "Sperf GC per node"
-    "$sperfpath" -x -d "$subdiag" core gc -r nodes > ./wrapper/sperfnodegc.txt &
-    echo "Sperf StatusLogger per node"
-    "$sperfpath" -x -d "$subdiag" core statuslogger -r histogram > ./wrapper/sperfnodestatuslog.txt &
-  fi
-  if [[ "$solrparse" == 1 ]]; then
-    echo "Sperf Solr Filtercache"
-    "$sperfpath" -x search filtercache -d "$subdiag" > ./wrapper/sperfsolrcache.txt &
-    echo "Sperf Solr Queryscore"
-    "$sperfpath" -x search queryscore -d "$subdiag" > ./wrapper/sperfsolrscore.txt &
-  fi
+  sperfcmd="$sperfpath"
 fi
+
+echo "Sperf summary"
+${sperfcmd} -x -d "$subdiag" > ./wrapper/sperf1general.txt &
+echo "Sperf GC analysis"
+${sperfcmd} -x -d "$subdiag" core gc > ./wrapper/sperf2gc.txt &
+echo "Sperf StatusLogger"
+${sperfcmd} -x -d "$subdiag" core statuslogger > ./wrapper/sperf3statuslog.txt &
+echo "Sperf Slow Query"
+${sperfcmd} -x core slowquery -d "$subdiag" > ./wrapper/sperf4slow.txt &
+echo "Sperf Schema"
+${sperfcmd} -x core schema -d "$subdiag" > ./wrapper/sperf5schema.txt &
+if [[ "$pernode" == 1 ]]; then
+  echo "Sperf GC per node"
+  ${sperfcmd} -x -d "$subdiag" core gc -r nodes > ./wrapper/sperf6nodegc.txt &
+  echo "Sperf StatusLogger per node"
+  ${sperfcmd} -x -d "$subdiag" core statuslogger -r histogram > ./wrapper/sperf7nodestatuslog.txt &
+fi
+if [[ "$solrparse" == 1 ]]; then
+  echo "Sperf Solr Filtercache"
+  ${sperfcmd} -x search filtercache -d "$subdiag" > ./wrapper/sperf8solrcache.txt &
+  echo "Sperf Solr Queryscore"
+  ${sperfcmd} -x search queryscore -d "$subdiag" > ./wrapper/sperf9solrscore.txt &
+fi
+
 wait
+}
+
+slgrep() {
+if [[ ! -d "$opscdiag"/slg ]]; then
+	echo "Creating slg directory"
+	pwd
+	mkdir "$opscdiag"/slg
+fi
+
+if [[ -f $grepsl && $slgreps == 1 ]]; then
+  echo ; echo "Running SL greps"
+  $grepsl -c "$opscdiag" &
+  $grepsl -g "$opscdiag" &
+  $grepsl -s "$opscdiag" &
+  $grepsl -6 "$opscdiag" &
+  $grepsl -t "$opscdiag" &
+  $grepsl -q "$opscdiag" &
+  $grepsl -b "$opscdiag" &
+  $grepsl -l "$opscdiag" &
+  wait
+else
+  echo "ERROR: Grep script not found. Skipping..."
+fi
+}
+
+diagviewer() {
+  if [[ $diagv == 1 ]]; then
+    export PYTHONPATH=$PYTHONPATH:$jbd/src
+    $dvpy $dvpath/src/import $opscdiag
+    echo "Run the following command to review the content of the diagviewer or access the sqlite db" > ./wrapper/diag-viewer.txt
+    echo "cd $dvpath/src" >> ./wrapper/diag-viewer.txt
+    echo "python -mviewer $opscdiag/diagnostics.db" >> ./wrapper/diag-viewer.txt
+    # echo "DB is available under $opscdiag/diagnostics.db" >> ./wrapper/diag-viewer.txt
+  fi
+  }
+
+montecristo(){
+  if [ -f "$opscdiag/../$diagtgz" ] && [ -n "$ticketid" ] && [ $montecris == 1 ]; then
+    export JAVA_HOME=$jvmh
+    mkdir "$opscdiag"/../diagMC-$ticketid
+    cp "$opscdiag/../$diagtgz" "$opscdiag"/../diagMC-$ticketid
+    pushd "$mcpath"
+    #pushd "$opscdiag"/../diagMC-$ticketid
+    $mcpath/run.sh -d -s -c "$opscdiag"/../diagMC-$ticketid $ticketid | tee ./mctmp.txt
+    mv -f ./mctmp.txt "$opscdiag"/wrapper/mc.txt
+    popd
+  else
+    echo "MonteCristo requires both diag tgz file (-m) and ticket id (-t)"
+    echo "Is the file "$opscdiag"/../"$diagtgz" available along ticket number?"
+  fi
+}
+
+converter(){
+  if [ -f $HOME/ds-discovery/$ticketid/reports/montecristo/content/exporter.md ]; then
+    python "$template"/md2html.py $HOME/ds-discovery/$ticketid/reports/montecristo/content/exporter.md "$opscdiag"/wrapper/mc.html
+  else
+    echo ERROR: MonteCristo failed to generate the summary exporter.md, review above for specific issues on its execution
+  fi
 }
 
 # Time to build the content
@@ -160,21 +218,50 @@ cat > ./wrapper/left_frame.htm << EOF
    </head>
 
    <body>
-     <img src="./datastax.png" alt="DataStax"><br><br>
+     <img src="./ds.svg" alt="DataStax"><br>
 EOF
 }
 
 # Populate the frame for nibbler files
 nibblerpop() {
+for i in $(ls ./Nibbler/*.out | grep -E -v '[1-3]')
+do
+  cat > ${i%.*}.htm << EOF
+<html>
+<head>
+    <style>
+        body {
+            white-space: pre; /* Prevent text wrapping */
+            font-family: monospace; /* Use a monospaced font for better readability */
+        }
+    </style>
+</head>
+<body>
+EOF
+  cat ${i%.*}.out >> ${i%.*}.htm
+  cat >> ${i%.*}.htm << EOF
+</body>
+</html>
+EOF
+done
+
 cat >> ./wrapper/left_frame.htm << EOF
      <b>Nibbler</b><br>
 EOF
 
-for i in $(ls ./Nibbler)
-do
-  linkname
-	printf '\t\t\t<a href="../Nibbler/%s" target = "center">%s</a><br>\n' $i $link >> ./wrapper/left_frame.htm
-done
+if [[ $nowrap == 1 ]]; then
+    for i in $(ls ./Nibbler/*.htm | xargs -n 1 basename | sed 's/.htm$//')
+    do
+      linkname
+      printf '\t\t\t<a href="../Nibbler/%s" target = "center">%s</a><br>\n' ${i%.*}.htm $link >> ./wrapper/left_frame.htm
+    done
+else
+    for i in $(ls ./Nibbler/*.out | xargs -n 1 basename | sed 's/.out$//')
+    do
+      linkname
+      printf '\t\t\t<a href="../Nibbler/%s" target = "center">%s</a><br>\n' ${i%.*}.out $link >> ./wrapper/left_frame.htm
+    done
+fi
 }
 
 # Populate the frame for sperf files
@@ -184,12 +271,39 @@ cat >> ./wrapper/left_frame.htm << EOF
     <b>Sperf</b><br>
 EOF
 
-for i in $(ls -tr ./wrapper | grep sperf*)
+for i in $(ls ./wrapper | grep sperf*)
 do
   # echo $i
   linkname
 	printf '\t\t\t<a href="./%s" target = "center">%s</a><br>\n' $i $link >> ./wrapper/left_frame.htm
 done
+}
+
+# SL greps
+grepspop() {
+cat >> ./wrapper/left_frame.htm << EOF
+      <br>
+    <b>Greps</b><br>
+EOF
+
+for i in $(ls ./slg | grep -E '[1-3]')
+do
+  # echo $i
+  linkname
+	printf '\t\t\t<a href="../slg/%s" target = "center">%s</a><br>\n' $i $link >> ./wrapper/left_frame.htm
+done
+}
+
+# Diag link
+dvpop() {
+cat >> ./wrapper/left_frame.htm << EOF
+      <br>
+    <b>DiagV & MC</b><br>
+EOF
+
+  printf '\t\t\t<a href="./%s" target = "center">%s</a><br>\n' diag-viewer.txt DVinfo >> ./wrapper/left_frame.htm
+  echo "Come back later, I'm working. MonteCristo generation in progress. Data will be in http://127.0.0.1:1313" > ./wrapper/mc.txt
+  printf '\t\t\t<a href="./%s" target = "center">%s</a><br>\n' mc.html MC >> ./wrapper/left_frame.htm
 }
 
 footer() {
@@ -244,6 +358,10 @@ elif [[ -d $(find "$opscdiag" -mindepth 2 -maxdepth 2 -name 'nodes' -type d) ]];
 elif [[ -d $(find "$opscdiag" -mindepth 1 -maxdepth 1 -name 'nodes' -type d) ]]; then
   # Directly in the directory containing the nodes folder. Setting subdiag as per the current diag.
   subdiag="$opscdiag"
+elif [[ -d $(find "$opscdiag" -mindepth 1 -maxdepth 1 -name '*artifacts*' -type d | head -1) ]]; then
+  # Directly in the directory containing artifacts (Services ds-collector)
+  echo "Detected services collector. Let's make a prayer together"
+  subdiag="$opscdiag"
 else
   echo "Cannot find the nodes folder. Exiting..."
   exit 1
@@ -273,15 +391,23 @@ else
   prep
   nibblerrun
   sperfrun
+  slgrep
+  diagviewer
   header
   nibblerpop
   sperfpop
+  grepspop
+  dvpop
   footer
   # need to revisit tarball generation. Too many issues at the moment
   # tarball
   browseropen
+  montecristo
+  converter
 fi
 
-if [[ "$debug" == "1" ]]; then debuginfo
+if [[ "$debug" == "1" ]]; then 
+  debuginfo
 fi
+
 popd > /dev/null
